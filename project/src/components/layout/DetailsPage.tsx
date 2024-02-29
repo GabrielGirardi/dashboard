032e69b7d72a1b5b803eeb16df85c09296e63d49
Gabriel Girardi <gabrielgirardi@Gabriels-MacBook-Pro.local>
2024-02-29 18:23:38 -0300

    feat: 🎸 Criação de componentes iniciais Dashboard

---
+++ project/src/components/layout/DetailsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DetailsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Detalhes do Item</h1>
+      <p>Esta é a página de detalhes do item.</p>
+    </div>
+  );
+};
+
+export default DetailsPage;
